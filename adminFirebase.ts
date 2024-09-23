87d6ba0ff0dce3c6403755c03bb28c1ad8be2b46
Freelancer0708 <freelancernoaccont@gmail.com>
2024-09-23 17:31:36 +0900

    update firebase settings

--- adminFirebase.ts
+++ adminFirebase.ts
@@ -1,11 +1,17 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth, setPersistence, browserLocalPersistence } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import { adminFirebaseConfig } from './adminFirebaseConfig';
 
 let adminApp;
 if (!getApps().some(app => app.name === 'adminApp')) {
-    adminApp = initializeApp(adminFirebaseConfig, 'adminApp');
+    adminApp = initializeApp({
+        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY_ADMIN,
+        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN_ADMIN,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID_ADMIN,
+        storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET_ADMIN,
+        messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID_ADMIN,
+        appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID_ADMIN
+    }, 'adminApp');
 } else {
     adminApp = getApp('adminApp');
 }
--- userFirebase.ts
+++ userFirebase.ts
@@ -1,11 +1,17 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth, setPersistence, browserLocalPersistence } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import { userFirebaseConfig } from './userFirebaseConfig';
 
 let userApp;
 if (!getApps().some(app => app.name === 'userApp')) {
-    userApp = initializeApp(userFirebaseConfig, 'userApp');
+    userApp = initializeApp({
+        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY_USER,
+        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN_USER,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID_USER,
+        storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET_USER,
+        messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID_USER,
+        appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID_USER
+    }, 'userApp');
 } else {
     userApp = getApp('userApp');
 }
